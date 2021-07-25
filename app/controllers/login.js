d0b726e9c4aa543890d040e9d861428bf0d4eb24
Unknown <achrafatauil@hotmail.com>
2021-07-25 19:26:08 +0200

    add login, register & index controller

---
+++ app/controllers/login.js
@@ -0,0 +1,28 @@
+import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+
+export default class LoginController extends Controller {
+  @service session;
+
+  @tracked email;
+  @tracked password;
+  @tracked email;
+  @tracked error = [];
+
+  @action
+    async login(event) {
+      event.preventDefault();
+      this.error = [];
+
+      try {
+        await this.session.authenticate('authenticator:mu-semtech', {
+          email: this.email,
+          password: this.password
+        });
+      } catch(err){
+        this.error = err.errors[0].detail;
+      }
+    }
+}
